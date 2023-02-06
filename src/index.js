b541e2f3f8d8a43bc6079c51f2fb924653e8ea61
Ruslan Berezko <68183783+Humagets@users.noreply.github.com>
2023-02-06 16:44:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][0]) {
+        stack.push(bracketsConfig[j][1]);
+        break;
+      } else if (str[i] === bracketsConfig[j][1]) {
+        if (stack.pop() !== str[i]) {
+          return false;
+        }
+        break;
+      }
+    }
+  }
+  return stack.length === 0;
+};
